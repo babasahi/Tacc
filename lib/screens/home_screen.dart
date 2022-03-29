24edbf1c9abe8bf2fffb11a6377531819960a25e
Babe Saleh Mahfoud <babesalehmahfoud@gmail.com>
2022-03-29 20:58:53 +0000

    changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SafeArea(child: Container()),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              child: Container(
+                height: 22,
+                width: 12,
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent.withOpacity(0.4),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Text('Play Now !'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
