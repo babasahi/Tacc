3e2e4e770c31f8a1866961adf0894b746a9c5366
Babe Saleh Mahfoud <babesalehmahfoud@gmail.com>
2022-03-29 20:54:39 +0000

    improvements

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Container()),
+    );
   }
 }
