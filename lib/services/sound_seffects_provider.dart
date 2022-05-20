d1771a8bcbd646674376f0a93148735327cb12ba
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-05-20 20:32:17 +0100

    added sounds folder

--- lib/services/sound_seffects_provider.dart
+++ lib/services/sound_seffects_provider.dart
@@ -1,5 +1,10 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class SoundEffects extends ChangeNotifier {
-  void playSound() {}
+  final AudioCache player = AudioCache();
+
+  void playSound() {
+    player.play('fileName');
+  }
 }
